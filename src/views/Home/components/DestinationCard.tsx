0ebb1c99b28eb3d6f068ea4c2f21f3ccecff20d1
hien.hoangwerner@hcmut.edu.vn <hien.hoangwerner@hcmut.edu.vn>
2024-04-15 02:09:32 +0700

    feat: responsive homepage

--- src/views/Home/components/DestinationCard.tsx
+++ src/views/Home/components/DestinationCard.tsx
@@ -12,10 +12,10 @@ const DestinationCard: FC<DestinationCardProps> = ({
     size = '1/3',
 }) => {
     return (
-        <div className={`relative destination-card w-${size}`}>
+        <div className={`relative destination-card w-${size} min-[800px]:text-xl min-[600px]:text-base min-[400px]:text-sm text-[6px]`}>
             <img src={imagePath} alt={title} className="rounded-lg" />
             <div className="destination-card__title absolute top-0 left-0 w-full h-full bg-black opacity-40 rounded-lg"></div>
-            <div className="destination-card__title text-white text-center text-xl absolute bottom-[16px] left-[50%] translate-x-[-50%] font-[500]">
+            <div className="destination-card__title text-white text-center absolute bottom-[16px] left-[50%] translate-x-[-50%] font-[500]">
                 {title}
             </div>
         </div>
--- src/views/Home/components/Destinations.jsx
+++ src/views/Home/components/Destinations.jsx
@@ -5,15 +5,16 @@ import routeConstants from 'route/routeConstants';
 const Destinations = () => {
     const navigate = useNavigate();
     return (
-        <div className="w-11/12 mx-auto flex flex-col mb-20 mt-10">
-            <div className="w-full flex flex-row justify-between items-center py-4">
-                <div className="w-1/3">
-                    <div className="text-gray-500 font-[600]">DESTINATIONS</div>
-                    <div className="text-2xl font-[700]">
+        <div className="flex flex-col ">
+            <div className="w-full flex flex-row justify-between py-6">
+                <div className="w-1/3 min-[800px]:text-2xl min-[600px]:text-xl min-[400px]:text-base text-xs 
+                flex flex-col h-full justify-between space-y-3">
+                    <div className="text-gray-500 font-[600] flex ">DESTINATIONS</div>
+                    <div className="font-[700] flex">
                         City escapes and nature retreats
                     </div>
                 </div>
-                <div className="w-1/3">
+                <div className="w-1/3 min-[800px]:text-xl min-[600px]:text-base min-[400px]:text-sm text-[8px]">
                     <div className="text-gray-500">
                         From vibrant urban escapes to tranquil natural wonders,
                         find your perfect destination. Explore diverse
@@ -28,7 +29,7 @@ const Destinations = () => {
                 </div>
             </div>
             <div className="flex flex-col gap-6">
-                <div className="flex flex-row w-[calc(100%-24px)] gap-6">
+                <div className="flex flex-row gap-6">
                     <DestinationCard
                         imagePath={require('assets/image/destinations/hochiminh.webp')}
                         title="Ho Chi Minh City"
@@ -41,7 +42,7 @@ const Destinations = () => {
                     />
                 </div>
 
-                <div className="flex flex-row w-[calc(100%-24px)] gap-6">
+                <div className="flex flex-row gap-6">
                     <DestinationCard
                         imagePath={require('assets/image/destinations/mekongdelta.webp')}
                         title="Mekong Delta"
