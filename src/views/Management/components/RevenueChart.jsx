3d0d970a14469c4fc021c4d8528ace112a4aff48
Phạm Duy Quang <75131380+ULTIMATE-Mystery@users.noreply.github.com>
2024-05-25 05:51:34 +0700

    feat: enhance UI of Owner dashboard - Revenue Analytics chart

--- src/views/Management/components/RevenueChart.jsx
+++ src/views/Management/components/RevenueChart.jsx
@@ -7,6 +7,7 @@ const CustomTooltip = ({ active, payload, label }) => {
     return (
       <div className="custom-tooltip p-2 bg-gray-800 text-white rounded">
         <p>{`Revenue: $${payload[0].value}`}</p>
+        <p>{`Date: ${label}`}</p>
       </div>
     );
   }
@@ -20,16 +21,26 @@ const RevenueChart = () => {
     return <p>Loading...</p>;
   }
 
+  if (!rentData || rentData.length === 0) {
+    return <p>No data available</p>;
+  }
+
   const isSingleMonth = rentData.length === 1;
+  const singleMonthData = isSingleMonth ? rentData[0].dates : [];
+
+  // Check for unique dates within the single month
+  const uniqueDates = new Set(singleMonthData.map(date => date.date)).size;
+  const isSingleDate = uniqueDates === 1;
+  const isMultipleDates = uniqueDates > 1;
 
   return (
     <div className="w-full h-full py-8">
       <ResponsiveContainer width="100%" height="100%">
-        {isSingleMonth ? (
+        {isSingleMonth && isSingleDate ? (
           <BarChart
             width={400}
             height={240}
-            data={rentData}
+            data={singleMonthData}
             margin={{
               top: 10,
               right: 30,
@@ -37,11 +48,28 @@ const RevenueChart = () => {
               bottom: 0,
             }}
           >
-            <XAxis dataKey="monthYear" />
+            <XAxis dataKey="date" />
             <YAxis />
             <Tooltip content={<CustomTooltip />} />
             <Bar dataKey="rentAmount" fill="#77E2AB" barSize={60} />
           </BarChart>
+        ) : isSingleMonth && isMultipleDates ? (
+          <AreaChart
+            width={400}
+            height={240}
+            data={singleMonthData}
+            margin={{
+              top: 10,
+              right: 30,
+              left: 0,
+              bottom: 0,
+            }}
+          >
+            <XAxis dataKey="date" />
+            <YAxis />
+            <Tooltip content={<CustomTooltip />} />
+            <Area type="monotone" dataKey="rentAmount" stackId="1" stroke="#77E2AB" fill="#77E2AB" />
+          </AreaChart>
         ) : (
           <AreaChart
             width={400}
