f244327a42dbaf09960656f6531dd3723c4243c7
Phạm Duy Quang <75131380+ULTIMATE-Mystery@users.noreply.github.com>
2024-05-25 05:20:02 +0700

    feat: enhance UI, responsiveness for mobile for User dashboard in Management page

--- src/views/Management/components/UserDashboard.jsx
+++ src/views/Management/components/UserDashboard.jsx
@@ -78,13 +78,13 @@ const UserDashboard = ({ mode, setMode }) => {
       <div className='w-full p-6 md:p-10 justify-between space-y-8 md:space-y-12 '>
         <div className='w-full flex justify-between space-x-4'>
           <button onClick={() => { if (!isLoading) setMode("owner"); }}
-            className="w-fit px-4 py-2 md:px-6 md:py-3 text-white font-semibold bg-gradient-to-r from-indigo-700 via-purple-500 to-pink-600 rounded-lg shadow-lg hover:scale-105 duration-200 hover:drop-shadow-2xl hover:shadow-[#7dd3fc] hover:cursor-pointer"
+            className="w-fit px-4 py-2 md:px-6 md:py-3 text-white font-semibold bg-gradient-to-r from-indigo-700 h-[100%] via-purple-500 to-pink-600 rounded-lg shadow-lg hover:scale-105 duration-200 hover:drop-shadow-2xl hover:shadow-[#7dd3fc] hover:cursor-pointer"
             disabled={isLoading}>
             Switch to owner's dashboard
           </button>
           <div className="flex">
             <button onClick={clearLogs}
-              className={`w-fit border hover:scale-95 duration-300 relative group cursor-pointer text-sky-50 overflow-hidden h-[44px] w-28 md:w-36 my-auto rounded-md bg-sky-200 p-2 flex justify-center items-center font-extrabold ${(!checkoutSuccessful && lastSuccessfulContractId === -1) || isLoading ? 'opacity-50 cursor-not-allowed' : ''}`} disabled={isLoading}>
+              className={`w-fit border hover:scale-95 duration-300 relative group cursor-pointer text-sky-50 overflow-hidden h-[100%] md:w-full my-auto rounded-md bg-sky-200 p-2 flex justify-center items-center font-extrabold ${(!checkoutSuccessful && lastSuccessfulContractId === -1) || isLoading ? 'opacity-50 cursor-not-allowed' : ''}`} disabled={isLoading}>
               <div className="absolute right-32 -top-4 group-hover:top-1 group-hover:right-2 z-10 w-40 h-40 rounded-full group-hover:scale-150 duration-500 bg-sky-900"></div>
               <div className="absolute right-2 -top-4 group-hover:top-1 group-hover:right-2 z-10 w-32 h-32 rounded-full group-hover:scale-150 duration-500 bg-sky-800"></div>
               <div className="absolute -right-12 top-4 group-hover:top-1 group-hover:right-2 z-10 w-24 h-24 rounded-full group-hover:scale-150 duration-500 bg-sky-700"></div>
@@ -96,14 +96,14 @@ const UserDashboard = ({ mode, setMode }) => {
         <div className='text-2xl md:text-4xl font-bold text-slate-400 text-center'>
           Check out
         </div>
-        <div className='flex flex-col md:flex-row md:space-x-10 w-full justify-between'>
+        <div className='flex flex-col md:flex-row md:space-x-10 w-full justify-between items-center'>
           <div className='text-lg md:text-xl text-slate-400 flex my-auto mb-4'>
             First, select the contract of your rental room that you want to check out.
           </div>
-          <div className="flex items-center justify-center space-x-3">
+          <div className="flex items-center justify-end w-full md:w-auto">
             <div className="font-bold text-slate-400 text-sm md:text-base">{contractId > -1 ? `NFT ID: ${contractId}` : 'No contract selected'}</div>
             <button onClick={() => { if (!isLoading) setModalCheckoutOpened(true); }}
-               className={`w-fit border hover:scale-95 duration-300 relative group cursor-pointer text-sky-50 overflow-hidden h-[44px] w-28 md:w-36 my-auto rounded-md bg-sky-200 p-2 flex justify-center items-center font-extrabold ${isLoading ? 'opacity-50 cursor-not-allowed' : ''}`} disabled={isLoading}>
+               className={`ml-4 w-fit border hover:scale-95 duration-300 relative group cursor-pointer text-sky-50 overflow-hidden h-[44px] w-28 md:w-40 my-auto rounded-md bg-sky-200 p-2 flex justify-center items-center font-extrabold ${isLoading ? 'opacity-50 cursor-not-allowed' : ''}`} disabled={isLoading}>
                 <div className="absolute right-32 -top-4 group-hover:top-1 group-hover:right-2 z-10 w-40 h-40 rounded-full group-hover:scale-150 duration-500 bg-sky-900"></div>
               <div className="absolute right-2 -top-4 group-hover:top-1 group-hover:right-2 z-10 w-32 h-32 rounded-full group-hover:scale-150 duration-500 bg-sky-800"></div>
               <div className="absolute -right-12 top-4 group-hover:top-1 group-hover:right-2 z-10 w-24 h-24 rounded-full group-hover:scale-150 duration-500 bg-sky-700"></div>
@@ -144,7 +144,7 @@ const UserDashboard = ({ mode, setMode }) => {
           <div className='text-lg md:text-xl text-slate-400 flex my-auto mb-4'>
             After selection, you can check out.
           </div>
-          <div className="flex justify-center">
+          <div className="flex justify-center w-full md:w-auto">
             <button onClick={() => { if (!isLoading && newContractSelected) setModalConfirmOpened(true); }}
               disabled={!newContractSelected || isLoading}
               className={`w-fit px-8 py-2 md:px-12 md:py-3 text-white font-semibold bg-gradient-to-r from-blue-800 via-pink-400 via-purple-600 to-blue-600 shadow-lg hover:scale-110 duration-200 hover:drop-shadow-2xl hover:shadow-[#7dd3fc] hover:cursor-pointer
@@ -181,15 +181,15 @@ const UserDashboard = ({ mode, setMode }) => {
             </Modal>
           )}
         </div>
-        <div className='flex flex-col md:flex-row md:space-x-10 w-full justify-between'>
+        <div className='flex flex-col md:flex-row md:space-x-10 w-full justify-between items-center'>
           <div className='text-lg md:text-xl text-slate-400 flex my-auto mb-4'>
           After successfully checking out, you can review the room log here.
           </div>
-          <div className="flex items-center justify-center space-x-3">
+          <div className="flex items-center justify-end w-full md:w-auto">
             <div className="font-bold text-slate-400 text-sm md:text-base">{lastSuccessfulContractId > -1 ? `NFT ID: ${lastSuccessfulContractId}` : 'No room reviewed'}</div>
             <button onClick={() => { if (!isLoading) setModalReviewLogOpened(true); }}
               disabled={!checkoutSuccessful && lastSuccessfulContractId === -1 || isLoading}
-              className={`border hover:scale-95 duration-300 relative group cursor-pointer text-sky-50 overflow-hidden h-[44px] w-28 md:w-32 my-auto rounded-md bg-sky-200 p-2 flex justify-center items-center font-extrabold ${(!checkoutSuccessful && lastSuccessfulContractId === -1) || isLoading ? 'opacity-50 cursor-not-allowed' : ''}`}>
+              className={`ml-4 border hover:scale-95 duration-300 relative group cursor-pointer text-sky-50 overflow-hidden h-[44px] w-28 md:w-32 my-auto rounded-md bg-sky-200 p-2 flex justify-center items-center font-extrabold ${(!checkoutSuccessful && lastSuccessfulContractId === -1) || isLoading ? 'opacity-50 cursor-not-allowed' : ''}`}>
                 <div className="absolute right-32 -top-4 group-hover:top-1 group-hover:right-2 z-10 w-40 h-40 rounded-full group-hover:scale-150 duration-500 bg-sky-900"></div>
                 <div className="absolute right-2 -top-4 group-hover:top-1 group-hover:right-2 z-10 w-32 h-32 rounded-full group-hover:scale-150 duration-500 bg-sky-800"></div>
                 <div className="absolute -right-12 top-4 group-hover:top-1 group-hover:right-2 z-10 w-24 h-24 rounded-full group-hover:scale-150 duration-500 bg-sky-700"></div>
