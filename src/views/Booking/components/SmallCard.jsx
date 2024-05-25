6934f2ba4061d99d7aa28662c2acc0deb198198b
hien.hoangwerner@hcmut.edu.vn <hien.hoangwerner@hcmut.edu.vn>
2024-05-25 11:59:37 +0700

    feat: add convert bnb to usd

--- src/views/Booking/components/SmallCard.jsx
+++ src/views/Booking/components/SmallCard.jsx
@@ -17,6 +17,8 @@ const SmallCard = ({key,tokenId,page,contractId,select,setModalCheckoutOpened,pr
   // const { data, isLoading } = useContractRead(contract, "getNFTInfo", [tokenId]);
   const [dataToken,setDataToken] = useState();
   const [isViewClicked,setIsViewClicked] = useState(false);
+  const [convertedPrice, setConvertedPrice] = useState(null);
+
   const parseBigNumber = (value) => {
     return value ? value.toString() : "";
   
@@ -49,59 +51,64 @@ const SmallCard = ({key,tokenId,page,contractId,select,setModalCheckoutOpened,pr
       }
   `;
   const { loading:isLoading, error, data } = useQuery(GET_TOKENS);
-  useEffect(() => {
-    if (contract && address) {
-      getApprovedForAll();
+  const convertBnbToUsd = async (bnbPrice) => {
+    if (contract) {
+      try {
+        const amountUsd = await contract.call('bnbToUsd', [bnbPrice]);
+        return amountUsd;
+      } catch (error) {
+        console.error('Error calling bnbToUsd:', error);
+      }
     }
-    if (data) setDataToken(data);
-  }, [contract, address, data]);
+  };
+  
+  useEffect(() => {
+    const fetchConvertedPrice = async () => {
+      if (data) {
+        try {
+          const amountUsd = await convertBnbToUsd(price);
+          setConvertedPrice(amountUsd);
+        } catch (error) {
+          console.error('Error calling bnbToUsd:', error);
+        }
+      }
+    };
+    fetchConvertedPrice();
+  }, [data, price]);
+  
   console.log(tokenId,dataToken)
 
   return (
-    <>{!isLoading && data &&(
-      <div>
-    <div className="w-60 h-100 bg-slate-900 p-3 flex flex-col gap-1 rounded-2xl border border-sky-500 border-1 drop-shadow-lg"
-      onClick={()=>setIsViewClicked(true)}>
-      <div className="h-48 rounded-xl overflow-hidden">
-        <img
-          src={homestay4}
-          alt="Background"
-          className="w-full h-full object-cover"
-        />
-      </div>
-      <div class="flex flex-col gap-4 pb-4">
-        <div class="flex flex-row justify-between">
-          <div class="flex flex-col">
-            <span class="text-xl font-bold text-white">Alex Homestay </span>
-            <p class="text-sm text-slate-400">Room ID: {roomId}</p>
+    <>
+      {!isLoading && data && (
+        <div>
+          <div className="w-60 h-100 bg-slate-900 p-3 flex flex-col gap-1 rounded-2xl border border-sky-500 border-1 drop-shadow-lg" onClick={() => setIsViewClicked(true)}>
+            <div className="h-48 rounded-xl overflow-hidden">
+              <img src={homestay4} alt="Background" className="w-full h-full object-cover" />
+            </div>
+            <div className="flex flex-col gap-4 pb-4">
+              <div className="flex flex-row justify-between">
+                <div className="flex flex-col">
+                  <span className="text-xl font-bold text-white">Alex Homestay </span>
+                  <p className="text-sm text-slate-400">Room ID: {roomId}</p>
+                </div>
+                <span className="pt-0.5 font-bold text-green-600">{convertedPrice ? `${convertedPrice} $` : "0 $"}</span>
+              </div>
+              <button className={`${page === "booking" ? "hidden" : ""} hover:bg-sky-900 text-gray-50 bg-blue-950 py-2 rounded-md text-slate-300`} onClick={handleSelectContract}>
+                Select
+              </button>
+            </div>
           </div>
-          <span class="pt-0.5 font-bold  text-green-600">{price?price:"0 "}$</span>
+          {isViewClicked && (
+            <Modal open={isViewClicked} onCancel={() => setIsViewClicked(false)} onOk={() => setIsViewClicked(false)} width={1200} closable={false}>
+              <BookedCard tokenId={tokenId} page={page} isApprovedForAll={isApprovedForAll} queryData={data}></BookedCard>
+            </Modal>
+          )}
         </div>
-        {/* <button class="hover:bg-sky-900 text-gray-50 bg-blue-950 py-2 rounded-md text-slate-300"
-        onClick={()=>setIsViewClicked(true)}>
-          View details</button> */}
-          
-          <button class={`${page==="booking"?"hidden":""} hover:bg-sky-900 text-gray-50 bg-blue-950 py-2 rounded-md text-slate-300`}
-        onClick={handleSelectContract}>
-          Select
-          </button>
-      </div>
-      
-      </div>
-      {isViewClicked&&(
-      <Modal open={isViewClicked} onCancel={()=>setIsViewClicked(false)} 
-      onOk={()=>setIsViewClicked(false)} width={1200}
-      closable={false}>
-        <BookedCard tokenId={tokenId} page={page} isApprovedForAll={isApprovedForAll} queryData={data}>
-        </BookedCard>
-        </Modal>
       )}
-    </div>
-
-    )}
-    
     </>
-  )
+  );
+  
 }
 
 export default SmallCard
