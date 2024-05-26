7d3abcf9b09efc40601f4bfb0efd9a2787c4364f
hien.hoangwerner@hcmut.edu.vn <hien.hoangwerner@hcmut.edu.vn>
2024-05-27 00:16:13 +0700

    feat: update new ui of nft card

--- src/views/Booking/components/Booked.jsx
+++ src/views/Booking/components/Booked.jsx
@@ -65,7 +65,7 @@ const Booked = ({ isButtonClicked,page,contractId,selectContract,setModalCheckou
     return (
         <>
             <div className="flex rounded-xl text-base mt-8 relative">
-                <div className={`${page=="booking"?"min-[1580px]:grid-cols-5 min-[1400px]:grid-cols-4":""} m-[2px] rounded-xl w-full grid lg:grid-cols-3 min-[780px]:grid-cols-2 grid-cols-1 justify-between justify-items-center gap-10`}>
+                <div className={`${page=="booking"?"min-[1580px]:grid-cols-5 min-[1400px]:grid-cols-4 gap-24":"gap-12"} m-[2px] rounded-xl w-full grid lg:grid-cols-3 min-[780px]:grid-cols-2 grid-cols-1 justify-between justify-items-center `}>
                     {isLoading && <div className='absolute py-40 flex justify-center'>
                                         <Loading/>
                                     </div>}
